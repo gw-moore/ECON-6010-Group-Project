b2baca0ca021a449ff2e5f9385dc7fd010fd914f
bgilbert1 <gilberbo@mail.uc.edu>
2018-11-24 11:52:12 -0500

    Update Harrisons_regression_modeling_script.R

--- programs/r_programs/Harrisons_regression_modeling_script.R
+++ programs/r_programs/Harrisons_regression_modeling_script.R
@@ -22,4 +22,61 @@ summary(everything_model)
 AIC(everything_model)
 BIC(everything_model)
 
+# Getting Rid of some more variables
+regression_kosher1 <- c(regression_kosher <- c("cbsatitle", "year", "total_mig_in", "total_mig_out", "pop_rank", "non_us_citizens", "us_citizens", "income_rank", "msa_code", "geo.y", "gdp.x", "gdp.y")
+)
+
+# Regression seems too good to be true so I got rid of the observations where gdp was null. Brought it down to 303 observations
+modeling_df1 <- modeling_df %>% 
+  filter(gdp != "NA")
+head(modeling_df1)
+
+# AIC and BIC are much different
+AIC(everything_model);BIC(everything_model)
+AIC(gdp_not_null_model);BIC(gdp_not_null_model)
+
+modeling_reg1 <- modeling_df1[ , !(names(modeling_df1) %in% regression_kosher1)]
+gdp_not_null_model <- lm(data = modeling_reg1, total_mig_net ~ .)
+summary(gdp_not_null_model)
+
+# Residual Plots
+residual_plot_everything <- ggplot(lm(total_mig_net ~ ., data = modeling_reg)) +
+  geom_point(aes(x = .fitted, y = .resid)) +
+  geom_abline(modeling_reg, slope = 0, intercept = 0, color = "red")
+residual_plot_everything
+
+residual_plot1 <- ggplot(lm(total_mig_net~ ., data = modeling_reg1)) + 
+  geom_point(aes(x=.fitted, y=.resid)) + 
+  geom_abline(modeling_reg1, slope = 0, intercept = 0)
+residual_plot1
+
+# Trying another regression
+modeling_reg2 <- lm(data = modeling_df1, total_mig_net ~ gdp + age + income + mean_commute_minutes + 
+                      owner_occupied_housing_units + propertytax_1500to2000 + propertytax_3000over + 
+                      propertytax_800to1500 + propertytax_less800)
+summary(modeling_reg2)
+AIC(everything_model);BIC(everything_model)
+AIC(gdp_not_null_model);BIC(gdp_not_null_model)
+AIC(modeling_reg2);BIC(modeling_reg2)
+
+# Dropping income
+modeling_reg3 <- lm(data = modeling_df1, total_mig_net ~ gdp + age + mean_commute_minutes + 
+                      owner_occupied_housing_units + propertytax_1500to2000 + propertytax_3000over + 
+                      propertytax_800to1500 + propertytax_less800)
+summary(modeling_reg3)
+AIC(everything_model);BIC(everything_model)
+AIC(gdp_not_null_model);BIC(gdp_not_null_model)
+AIC(modeling_reg2);BIC(modeling_reg2)
+AIC(modeling_reg3);BIC(modeling_reg3)
+
+# AIC and BIC are lower with the third model
+# Residual Plot with last regression
+residual_plot2 <- ggplot(lm(data = modeling_df1, total_mig_net ~ gdp + age + mean_commute_minutes + 
+                              owner_occupied_housing_units + propertytax_1500to2000 + propertytax_3000over + 
+                              propertytax_800to1500 + propertytax_less800)) + 
+  geom_point(aes(x=.fitted, y=.resid)) + 
+  geom_abline(modeling_df1, slope = 0, intercept = 0)
+residual_plot2
+
+
 rm(list=ls())
